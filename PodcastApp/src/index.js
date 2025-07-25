83c67be9d9131ce0cfd7e7faa3186a696d029a33
Keathan01 <bredenkampk29@gmail.com>
2025-07-25 14:37:07 +0200

    fix: correct global.css import path for Tailwind build

--- PodcastApp/src/index.js
+++ PodcastApp/src/index.js
@@ -4,7 +4,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import "./index.css";
+import "./global.css";
+
 import { FilterProvider } from "./context/FilterContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
